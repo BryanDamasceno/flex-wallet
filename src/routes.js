8a1885920a96cfc4d83f6aee7f7935eb6799c2e8
Bryan Azevedo <bryan42damasceno@gmail.com>
2021-05-04 20:14:56 -0300

    ADD Home criada, titulo e fontes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/home" />
+        <Route path="/" exact component={Home} />
       </Switch>
     </BrowserRouter>
   );
