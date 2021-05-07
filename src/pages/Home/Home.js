f5f387a9a74000428b37d4a675a92e6adc3d9b4a
Machado <guilherme_henriqueoh@live.com>
2021-05-07 18:27:59 -0300

    FIX mudança dos botões e animação do carrossel de imagens

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,38 +11,59 @@ function Home() {
       </div>
 
       <div className="content">
+       <div class="caroussel">
+        <div class="slider">
+          <div class="slides">
+            <input type="radio" name="radio-btn" id="radio1"></input>
+            <input type="radio" name="radio-btn" id="radio2"></input>
+            <input type="radio" name="radio-btn" id="radio3"></input>
+            <input type="radio" name="radio-btn" id="radio4"></input>
 
-        <div className="caroussel">
-
-      <input type="radio" class="slide-controller" name="slide" id="slide0"></input>
- 
-      <input type="radio" class="slide-controller" name="slide" id="slide1"></input>
-    
-      <input type="radio" class="slide-controller" name="slide" id="slide2"></input>
-      
-      <input type="radio" class="slide-controller" name="slide" id="slide3"></input>
-      
-
-          <ul class="slider">
-            <li class ="slide">
-            <label for="slide0" class="inputlabel"></label>
-              <img src="img1.png" alt="" />
-            </li>
-            <li class ="slide">
-            <label for="slide1" class="inputlabel"></label>
-              <img src="img2.jpg" alt=""  />
-            </li>
-            <li class ="slide">
-            <label for="slide2" class="inputlabel"></label>
-              <img src="img3.jpg" alt=""  />
-            </li>
-            <li class ="slide">
-            <label for="slide3" class="inputlabel"></label>
-              <img src="img4.png" alt=""  />
-            </li>
-          </ul>
+            <div class="slide first">
+            <img src="img1.png" alt=""></img>
+            </div>
+            <div class="slide">
+            <img src="img2.jpg" alt=""></img>
+            </div>
+            <div class="slide">
+            <img src="img3.jpg" alt=""></img>
+            </div>
+            <div class="slide">
+            <img src="img4.png" alt=""></img>
+            </div> 
+            <div class="navigation-auto">
+              <div class="auto-btn1"></div>
+              <div class="auto-btn2"></div>
+              <div class="auto-btn3"></div>
+              <div class="auto-btn4"></div>
+            </div>
+          </div>
+          <div class="navigation-manual">
+            <label for="radio1" class="manual-btn"></label>
+            <label for="radio2" class="manual-btn"></label>
+            <label for="radio3" class="manual-btn"></label>
+            <label for="radio4" class="manual-btn"></label>
+          </div>
         </div>
 
+
+
+        {/*
+          Não sei porque isso não tá funcionando, é pra ser a parte que muda automático as fotos do carrosel.
+        <script>
+          int contador = 1;
+          setInterval(function(){
+            document.getElementById('radio' + contador).checked = true}
+            contador ++
+            if(contador > 4){
+              contador = 1
+            }
+            , 5000);
+        </script>
+      */}
+
+
+        </div>
         <div className="register">
           <Link to="cadastro">
             <button className="button">Cadastre-se</button>
