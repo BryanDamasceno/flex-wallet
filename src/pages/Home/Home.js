ce8a81e2f419ba70b3b2b388ecedf572da89ddf6
Bryan Azevedo <bryan42damasceno@gmail.com>
2021-05-08 23:43:03 -0300

    ADD vetor verde alinhado

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,67 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-
-
-var counter = 1;
-          setInterval(function(){
-          document.getElementById('radio' + counter).checked = true;
-          counter ++;
-         if(counter > 4){
-         counter = 1
-          }
-        }, 5000);
 
 function Home() {
   return (
     <div className="container">
-
-          <div className="header">
-           <img className="vectorgreen" src="vectorgreen.svg" alt=""></img>
-           <img className="vectorblue"  src="vectorblue.svg"  alt=""></img>
-           
-
       <div className="title">
         <h1>Flex Wallet</h1>
         <p>Uma forma fácil e descontraída de organizar suas finanças.</p>
       </div>
+      <div className="vectors">
+        <img className="vectorgreen" src="vectorgreen.svg" alt=""></img>
       </div>
-      <div className="content">
-       <div class="caroussel">
-        <div class="slider">
-          <div class="slides">
-            <input type="radio" name="radio-btn" id="radio1"></input>
-            <input type="radio" name="radio-btn" id="radio2"></input>
-            <input type="radio" name="radio-btn" id="radio3"></input>
-            <input type="radio" name="radio-btn" id="radio4"></input>
 
-            <div class="slide first">
-            <img src="img1.png" alt=""></img>
+      <div className="content">
+        <div class="caroussel">
+          <div class="slider">
+            <div class="slides">
+              <input type="radio" name="radio-btn" id="radio1"></input>
+              <input type="radio" name="radio-btn" id="radio2"></input>
+              <input type="radio" name="radio-btn" id="radio3"></input>
+              <input type="radio" name="radio-btn" id="radio4"></input>
+
+              <div class="slide first">
+                <img src="img1.png" alt=""></img>
+              </div>
+              <div class="slide">
+                <img src="img2.jpg" alt=""></img>
+              </div>
+              <div class="slide">
+                <img src="img3.jpg" alt=""></img>
+              </div>
+              <div class="slide">
+                <img src="img4.png" alt=""></img>
+              </div>
+              <div class="navigation-auto">
+                <div class="auto-btn1"></div>
+                <div class="auto-btn2"></div>
+                <div class="auto-btn3"></div>
+                <div class="auto-btn4"></div>
+              </div>
             </div>
-            <div class="slide">
-            <img src="img2.jpg" alt=""></img>
-            </div>
-            <div class="slide">
-            <img src="img3.jpg" alt=""></img>
-            </div>
-            <div class="slide">
-            <img src="img4.png" alt=""></img>
-            </div> 
-            <div class="navigation-auto">
-              <div class="auto-btn1"></div>
-              <div class="auto-btn2"></div>
-              <div class="auto-btn3"></div>
-              <div class="auto-btn4"></div>
+            <div class="navigation-manual">
+              <label for="radio1" class="manual-btn"></label>
+              <label for="radio2" class="manual-btn"></label>
+              <label for="radio3" class="manual-btn"></label>
+              <label for="radio4" class="manual-btn"></label>
             </div>
           </div>
-          <div class="navigation-manual">
-            <label for="radio1" class="manual-btn"></label>
-            <label for="radio2" class="manual-btn"></label>
-            <label for="radio3" class="manual-btn"></label>
-            <label for="radio4" class="manual-btn"></label>
-          </div>
-        </div>
-       
         </div>
         <div className="register">
           <Link to="cadastro">
