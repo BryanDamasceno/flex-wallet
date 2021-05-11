7f9663e5861e71d943178fc922710a1b08fd6752
Machado <guilherme_henriqueoh@live.com>
2021-05-11 20:01:03 -0300

    ADD iniciando a página de login e incluindo a rota

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,7 +5,25 @@ function Login() {
     return (
 
         <div className="Container">
+            <div className="title">
+                <h1>Login</h1>
+                <div className="login">
+                    <input type="email" placeholder="E-mail" className="email"></input>
+                    <input type="password" placeholder="Senha" className="senha"></input>
+                    <label>Esqueceu sua senha?</label>
+                    <input type="submit" name="acao" value="Login"></input>
+            </div>
+            </div>
+
+
+            <div className="vectors">
+                 <img className="vector" src="component.svg" alt=""></img>
+            </div>
+
             
+
         </div>
         );
 }
+
+export default Login;
