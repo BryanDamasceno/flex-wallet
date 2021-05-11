6c4c72632d3f43ac99efdf0195461db37d1c3c70
Machado <guilherme_henriqueoh@live.com>
2021-05-11 00:52:58 -0300

    ADD iniciando a página de Login

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,11 @@
+import "./Login.css";
+import { Link } from "react-router-dom";
+
+function Login() {
+    return (
+
+        <div className="Container">
+            
+        </div>
+        );
+}
